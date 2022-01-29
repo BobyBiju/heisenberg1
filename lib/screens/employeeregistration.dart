26c159b54b71e3ff6cba373fc288b7296bbc3ff9
ananthanrj <ananthakrishnanrj@gmail.com>
2022-01-29 23:43:40 +0530

    UI chnage employeeregistration Screen

--- lib/screens/employeeregistration.dart
+++ lib/screens/employeeregistration.dart
@@ -5,26 +5,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:joli/screens/createprofileemployee.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-
 class EmployeeRegistrationScreen extends StatefulWidget {
+  static String id = 'employee_registration_screen';
 
-  static String id='employee_registration_screen';
   @override
-  _EmployeeRegistrationScreenState createState() => _EmployeeRegistrationScreenState();
+  _EmployeeRegistrationScreenState createState() =>
+      _EmployeeRegistrationScreenState();
 }
 
-class _EmployeeRegistrationScreenState extends State<EmployeeRegistrationScreen> {
+class _EmployeeRegistrationScreenState
+    extends State<EmployeeRegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
 
-  final _auth=FirebaseAuth.instance;
-
-  bool showSpinner=false;
+  bool showSpinner = false;
   String email;
   String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: ModalProgressHUD(inAsyncCall: showSpinner,
+      backgroundColor: Color(0xFFF0F1F3),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
@@ -32,39 +34,46 @@ class _EmployeeRegistrationScreenState extends State<EmployeeRegistrationScreen>
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Flexible(
-                child: Hero(tag: 'logo',
+                child: Hero(
+                  tag: 'logo',
                   child: Container(
-                    height: 150.0,
-                    child: Image.asset('images/laborer.png'),
+                    height: 350.0,
+                    child: Image.asset('images/Employeee.png'),
                   ),
                 ),
               ),
               SizedBox(
-                height: 48.0,
+                height: 28.0,
               ),
-              TextField(textAlign: TextAlign.center,
+              TextField(
+                textAlign: TextAlign.center,
+                cursorColor: Colors.black45,
                 keyboardType: TextInputType.emailAddress,
                 style: TextStyle(
-                  color: Colors.white,
+                  color: Colors.black,
                 ),
                 onChanged: (value) {
-                  email=value;
+                  email = value;
                 },
                 decoration: kTextfieldDecoration.copyWith(
-                    hintText: 'Enter your email'
-                ),
+                    hintText: 'Enter your email',
+                    hintStyle: TextStyle(color: Colors.black45)),
               ),
               SizedBox(
-                height: 8.0,
+                height: 10.0,
               ),
-              TextField(textAlign: TextAlign.center,obscureText: true,
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                  onChanged: (value) {
-                    password=value;
-                  },
-                  decoration: kTextfieldDecoration.copyWith(hintText: 'Enter your 6 digit password')
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextfieldDecoration.copyWith(
+                    hintText: 'Enter your 6 digit password',
+                    hintStyle: TextStyle(color: Colors.black45)),
               ),
               SizedBox(
                 height: 24.0,
@@ -72,33 +81,38 @@ class _EmployeeRegistrationScreenState extends State<EmployeeRegistrationScreen>
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0),
                 child: Material(
-                  color: Colors.blueAccent,
+                  color: Color(0xFF172e4a),
                   borderRadius: BorderRadius.all(Radius.circular(30.0)),
                   elevation: 5.0,
                   child: MaterialButton(
-                    onPressed: () async{
+                    onPressed: () async {
                       setState(() {
-                        showSpinner=true;
+                        showSpinner = true;
                       });
 
                       try {
-                        final newUser = await _auth.createUserWithEmailAndPassword(
-                            email: email, password: password);
-                        if(newUser!=null){
-                          Navigator.pushNamed(context, EmployeeProfileCreation.id);
+                        final newUser =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        if (newUser != null) {
+                          Navigator.pushNamed(
+                              context, EmployeeProfileCreation.id);
                         }
                         setState(() {
-                          showSpinner=false;
+                          showSpinner = false;
                         });
-
-                      } catch(e)
-                      {print(e);}
+                      } catch (e) {
+                        print(e);
+                      }
                     },
                     minWidth: 200.0,
                     height: 42.0,
                     child: Text(
                       'Register',
-                      style: TextStyle(color: Colors.white),
+                      style: TextStyle(
+                          color: Color(0xFFF0F1F3),
+                          fontSize: 19,
+                          fontWeight: FontWeight.w700),
                     ),
                   ),
                 ),
