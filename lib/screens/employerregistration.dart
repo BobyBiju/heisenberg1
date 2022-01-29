f29e6abe9f528b41924b9d8672cf3e53ea1fb0fd
ananthanrj <ananthakrishnanrj@gmail.com>
2022-01-29 15:06:28 +0530

    UI change OpeningPage

--- lib/screens/employerregistration.dart
+++ lib/screens/employerregistration.dart
@@ -6,26 +6,28 @@ import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import 'employerdashboard.dart';
 
-
 class EmployerRegistrationScreen extends StatefulWidget {
+  static String id = 'employer_registration_screen';
 
-  static String id='employer_registration_screen';
   @override
-  _EmployerRegistrationScreenState createState() => _EmployerRegistrationScreenState();
+  _EmployerRegistrationScreenState createState() =>
+      _EmployerRegistrationScreenState();
 }
 
-class _EmployerRegistrationScreenState extends State<EmployerRegistrationScreen> {
+class _EmployerRegistrationScreenState
+    extends State<EmployerRegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
 
-  final _auth=FirebaseAuth.instance;
-
-  bool showSpinner=false;
+  bool showSpinner = false;
   String email;
   String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: ModalProgressHUD(inAsyncCall: showSpinner,
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
@@ -33,7 +35,8 @@ class _EmployerRegistrationScreenState extends State<EmployerRegistrationScreen>
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Flexible(
-                child: Hero(tag: 'logo',
+                child: Hero(
+                  tag: 'logo',
                   child: Container(
                     height: 150.0,
                     child: Image.asset('images/employer.png'),
@@ -43,30 +46,32 @@ class _EmployerRegistrationScreenState extends State<EmployerRegistrationScreen>
               SizedBox(
                 height: 48.0,
               ),
-              TextField(textAlign: TextAlign.center,
+              TextField(
+                textAlign: TextAlign.center,
                 keyboardType: TextInputType.emailAddress,
                 style: TextStyle(
                   color: Colors.white,
                 ),
                 onChanged: (value) {
-                  email=value;
+                  email = value;
                 },
-                decoration: kTextfieldDecoration.copyWith(
-                    hintText: 'Enter your email'
-                ),
+                decoration:
+                    kTextfieldDecoration.copyWith(hintText: 'Enter your email'),
               ),
               SizedBox(
                 height: 8.0,
               ),
-              TextField(textAlign: TextAlign.center,obscureText: true,
+              TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
                   style: TextStyle(
                     color: Colors.white,
                   ),
                   onChanged: (value) {
-                    password=value;
+                    password = value;
                   },
-                  decoration: kTextfieldDecoration.copyWith(hintText: 'Enter your 6 digit password')
-              ),
+                  decoration: kTextfieldDecoration.copyWith(
+                      hintText: 'Enter your 6 digit password')),
               SizedBox(
                 height: 24.0,
               ),
@@ -77,23 +82,24 @@ class _EmployerRegistrationScreenState extends State<EmployerRegistrationScreen>
                   borderRadius: BorderRadius.all(Radius.circular(30.0)),
                   elevation: 5.0,
                   child: MaterialButton(
-                    onPressed: () async{
+                    onPressed: () async {
                       setState(() {
-                        showSpinner=true;
+                        showSpinner = true;
                       });
 
                       try {
-                        final newUser = await _auth
-                            .createUserWithEmailAndPassword(
-                            email: email, password: password);
-                        if(newUser!=null){
+                        final newUser =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        if (newUser != null) {
                           Navigator.pushNamed(context, EmployerDashboard.id);
                         }
                         setState(() {
-                          showSpinner=false;
+                          showSpinner = false;
                         });
-                      } catch(e)
-                      {print(e);}
+                      } catch (e) {
+                        print(e);
+                      }
                     },
                     minWidth: 200.0,
                     height: 42.0,
